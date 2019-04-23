0c5ebc218e0e1baae4e607597933e90fa1aa0bde
SonicZaFan <42089305+SonicZaFan@users.noreply.github.com>
2019-04-23 18:21:23 +0200

    add Thread_Handle()

--- main.c
+++ main.c
@@ -11,3 +11,12 @@ void Main(){
 void Thread1(uint32_t activation){
 	initializeAudio(); // Audio Activation Routine
 }
+unsigned short Thread_Handle(unsigned short inp){
+	if(inp == 0x5D2A) return 0;
+	H1 = inp >> 16;
+	reutrn (unsigned short)H1;
+	if(t.timeexecuted == 0xA8C0){
+		// Easter Egg Routine, activates when player is in game 43200 seconds (or half of day)
+		gamtc = (uint8_t)0xFFFF; // Crash game instantly with "overflowing" value (yup.) 
+	}
+}
